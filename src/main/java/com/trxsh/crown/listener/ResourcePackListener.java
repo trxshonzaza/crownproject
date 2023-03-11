09a9a90140e3f8e1e7c39699bb6a192f57f19d75
BuildTools <unconfigured@null.spigotmc.org>
2023-03-11 12:33:38 -0500

    major improvements and bug fixes, now depends ProtocolLib

---
+++ src/main/java/com/trxsh/crown/listener/ResourcePackListener.java
@@ -0,0 +1,36 @@
+package com.trxsh.crown.listener;
+
+import com.trxsh.crown.Main;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
+
+public class ResourcePackListener implements Listener {
+
+    @EventHandler
+    public void onResourcePackStatus(PlayerResourcePackStatusEvent e) {
+
+        if(e.getStatus().equals(PlayerResourcePackStatusEvent.Status.DECLINED)) {
+
+            e.getPlayer().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD + "You must accept and download the resource pack to play on the server! \n" + ChatColor.GREEN + "" + ChatColor.BOLD + "need help? create a ticket in '#tickets'");
+
+        }
+
+        if(e.getStatus().equals(PlayerResourcePackStatusEvent.Status.FAILED_DOWNLOAD)) {
+
+            e.getPlayer().kickPlayer(ChatColor.RED + "" + ChatColor.BOLD + "You must accept and download the resource pack to play on the server! \n" + ChatColor.GREEN + "" + ChatColor.BOLD + "need help? create a ticket in '#tickets'");
+
+        }
+
+        if(e.getStatus().equals(PlayerResourcePackStatusEvent.Status.ACCEPTED)) {
+
+            e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, .5f);
+            e.getPlayer().sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Thank you for loading the texture pack! have fun!");
+
+        }
+
+    }
+
+}
---
+++ src/main/java/com/trxsh/crown/manager/TextureManager.java
@@ -0,0 +1,22 @@
+package com.trxsh.crown.manager;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.PacketContainer;
+import com.trxsh.crown.Main;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class TextureManager {
+
+    public static void LoadTexture(Player p) throws InvocationTargetException {
+
+        PacketContainer packet = new ProtocolLibrary().getProtocolManager().createPacket(PacketType.Play.Server.RESOURCE_PACK_SEND);
+
+        packet.getStrings().write(0, Main.url).write(1, "sha-1 sum");
+
+        ProtocolLibrary.getProtocolManager().sendServerPacket(p, packet);
+    }
+
+}
